2640aad35a1aafe7a9bac1c5fa8311fc93e7b7fc
Cooper Hanke <hanke31314@gmail.com>
2018-02-10 15:48:31 -0600

    added animated background colors

--- app/src/main/java/com/example/cooper/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/cooper/helloandroid/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.cooper.helloandroid;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout container;
+    AnimationDrawable anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        container = findViewById(R.id.container);
+
+        anim = (AnimationDrawable) container.getBackground();
+        anim.setEnterFadeDuration(6000);
+        anim.setExitFadeDuration(2000);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (anim != null && !anim.isRunning())
+            anim.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (anim != null && anim.isRunning())
+            anim.stop();
     }
 }
